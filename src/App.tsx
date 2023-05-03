d8150aa90e9d72fe7214dcddf4210818d4402c79
Ivanwg <81613195+Ivanwg@users.noreply.github.com>
2023-05-03 16:21:33 +0300

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import ScrollToTop from './utils/scrollToTop';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <ScrollToTop />
+        <Routes>
+          <Route path='/' element={ <Navigate to="/team" /> }/>
+            <Route path='/team' element={ <div></div>
+              // <Layout>
+              //   <Header />
+              //   <Content>
+              //     <CardsList />
+              //   </Content>
+              // </Layout>
+            }>
+              {/* <Route path='/team/:id' element={<Post />} /> */}
+            </Route>
+            <Route path='*' element={<Navigate to="/error" />} />
+            {/* <Route path='/error' element={<NotFound />} /> */}
+        </Routes>
+      </BrowserRouter>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
       </header>
     </div>
   );
