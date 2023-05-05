552265b3ace683495ddee09af48253c5df5447b4
Ivanwg <81613195+Ivanwg@users.noreply.github.com>
2023-05-05 07:04:34 +0300

    spinner styles editted

--- src/components/CardsList/index.tsx
+++ src/components/CardsList/index.tsx
@@ -40,7 +40,7 @@ const CardsList = () => {
   return ( 
     <>
       {
-        status === 'load' ? <Spinner /> :
+        status !== 'load' ? <Spinner additionalClassNames={['transformedUp']} /> :
         <div className={styles.wrap}>
           <ul className={styles.list}>
             {
